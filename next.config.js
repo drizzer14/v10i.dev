adbd9d5268601ac9ce448cdd903b190de9f851b3
drizzer14 <drizzer14@gmail.com>
2021-11-02 21:53:58 +0200

    feat: update `next-pwa` scope

--- next.config.js
+++ next.config.js
@@ -15,7 +15,6 @@ module.exports = compose(
     sw: 'service.worker.js',
     disable: process.env.NODE_ENV === 'development',
     runtimeCaching,
-    scope: process.env.BASE_URL,
     dynamicStartUrl: false,
     reloadOnOnline: false,
     buildExcludes: [/-manifest\.json$/, /chunks\/pages\/api\/.*/],
