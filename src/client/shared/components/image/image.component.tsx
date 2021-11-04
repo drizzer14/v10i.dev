fca0ad4e2e445979f444e5ba18c218a82e2f13ea
drizzer14 <drizzer14@gmail.com>
2021-11-04 23:38:06 +0200

    feat: update `Image` styles, add caption

--- src/client/shared/components/image/image.component.tsx
+++ src/client/shared/components/image/image.component.tsx
@@ -5,13 +5,20 @@ import * as Styled from './image.styles';
 type ImageProps = {
   src: string;
   alt: string;
+  title?: string;
   className?: string;
 };
 
-export const Image: FC<ImageProps> = ({ src, alt, className }) => {
+export const Image: FC<ImageProps> = ({ src, alt, title, className }) => {
   return (
     <Styled.Image className={className}>
       <img src={src} alt={alt} />
+
+      {title && (
+        <Styled.Caption>
+          <small>{title}</small>
+        </Styled.Caption>
+      )}
     </Styled.Image>
   );
 };
--- src/client/shared/components/image/image.styles.ts
+++ src/client/shared/components/image/image.styles.ts
@@ -1,34 +1,23 @@
 import styled from 'styled-components';
 
-import { nonMobile, nonMobileWithGap } from '@/shared/mixin';
-
 export const Image = styled.figure`
-  position: relative;
-  left: -2rem;
-
-  width: calc(100% + 4rem);
-  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   margin-bottom: 1rem;
 
-  overflow: hidden;
-
-  &,
-  & > img {
-    transition: none;
-  }
-
-  ${nonMobile`
-    border-radius: 0.5rem;
-  `}
-
-  ${nonMobileWithGap`
-    position: initial;
-
-    width: 100%;
-  `}
-
   & > img {
     object-fit: cover;
+
+    border-radius: 0.5rem;
   }
+`;
+
+export const Caption = styled.figcaption`
+  margin-top: 0.5rem;
+
+  color: var(--faint-strong-text);
+
+  text-align: center;
 `;
