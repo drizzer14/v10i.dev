1f53cdb0d6396f53a26a9554da2447e824a3ff8e
drizzer14 <drizzer14@gmail.com>
2021-11-08 23:01:49 +0200

    fix(markdown): update component types

--- src/client/shared/components/markdown/list/list-item/list-item.component.tsx
+++ src/client/shared/components/markdown/list/list-item/list-item.component.tsx
@@ -1,8 +1,9 @@
-import { FC, ReactNode, useMemo } from 'react';
+import { ReactNode, useMemo } from 'react';
+import type { LiComponent } from 'react-markdown/lib/ast-to-react';
 
 import * as Styled from './list-item.styles';
 
-export const ListItem: FC = ({ children }) => {
+export const ListItem: LiComponent = ({ children }) => {
   const parsedChildren = useMemo<ReactNode>(() => {
     const normalizedChildren = Array.isArray(children)
       ? children.filter((child) => child !== '\n')
--- src/client/shared/components/markdown/markdown.config.tsx
+++ src/client/shared/components/markdown/markdown.config.tsx
@@ -1,7 +1,11 @@
 import remarkGfm from 'remark-gfm';
 import remarkToc from 'remark-toc';
+import type {
+  Text,
+  OrderedListComponent,
+  UnorderedListComponent,
+} from 'react-markdown/lib/ast-to-react';
 import remarkUnwrapImages from 'remark-unwrap-images';
-import type { Text } from 'react-markdown/lib/ast-to-react';
 import { uriTransformer } from 'react-markdown/lib/uri-transformer';
 import type { ReactMarkdownOptions } from 'react-markdown/lib/react-markdown';
 
@@ -12,8 +16,8 @@ import { Code } from './code';
 import { Heading } from './heading';
 import { Divider } from './divider';
 import { List, ListItem } from './list';
-import { Blockquote } from './blockquote';
 import { makeSlug } from './shared/utils';
+import { Blockquote } from './blockquote';
 import { Anchor } from './shared/components';
 
 export const markdownConfig: Omit<ReactMarkdownOptions, 'children'> = {
@@ -29,30 +33,20 @@ export const markdownConfig: Omit<ReactMarkdownOptions, 'children'> = {
     return href.startsWith('#') ? undefined : '_blank';
   },
   components: {
-    // @ts-ignore: Incorrect library typings
     h1: Heading,
-    // @ts-ignore: Incorrect library typings
     h2: Heading,
-    // @ts-ignore: Incorrect library typings
     h3: Heading,
-    // @ts-ignore: Incorrect library typings
     h4: Heading,
-    // @ts-ignore: Incorrect library typings
     h5: Heading,
-    // @ts-ignore: Incorrect library typings
+    // @ts-ignore: incompatible library types
     a: Anchor,
-    // @ts-ignore: Incorrect library typings
-    ol: List,
-    // @ts-ignore: Incorrect library typings
-    ul: List,
+    ol: List as OrderedListComponent,
+    ul: List as UnorderedListComponent,
     li: ListItem,
-    // @ts-ignore: Incorrect library typings
     hr: Divider,
-    // @ts-ignore: Incorrect library typings
     blockquote: Blockquote,
-    // @ts-ignore: Incorrect library typings
+    // @ts-ignore: incompatible library types
     img: Image,
-    // @ts-ignore: Incorrect library typings
     pre: Pre,
     code: ({ children, inline, className }) => {
       const [, language] = /language-(\w+)/.exec(className || '') || [];
