23120f49e036ce880470b1f211b6f3efea985ff2
drizzer14 <drizzer14@gmail.com>
2022-01-03 17:26:57 +0200

    feat: add Google Analytics

--- src/client/core/app/scripts/scripts.component.tsx
+++ src/client/core/app/scripts/scripts.component.tsx
@@ -1,10 +1,33 @@
 import type { FC } from 'react';
 import Script from 'next/script';
 
+import { matchEnv } from 'shared/utils';
+
 export const Scripts: FC = () => {
   return (
     <>
       <Script src="/polyfills/smoothscroll.js" />
+
+      {matchEnv({
+        production: () => (
+          <>
+            <Script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-B0LGL8KKJ4"
+            />
+
+            <Script
+              dangerouslySetInnerHTML={{
+                __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B0LGL8KKJ4');`,
+              }}
+            />
+          </>
+        ),
+      })}
     </>
   );
 };
